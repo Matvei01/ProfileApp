7f63551690dbc75e524ca0bb7dccd8949b98f317
Matvei <khlestov.matvei@mail.ru>
2024-03-31 16:06:08 +0500

    Created base project, removed storyboard references

---
+++ ProfileApp/ProfileApp/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  ProfileApp
+//
+//  Created by Matvei Khlestov on 31.03.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
---
+++ ProfileApp/ProfileApp/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  ProfileApp
+//
+//  Created by Matvei Khlestov on 31.03.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = ProfileViewController()
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
